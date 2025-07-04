5e9c1f23d275f83f2d5d7da4417400e4d59c2a37
faisal-990 <sawezfaisals123@gmail.com>
2025-07-04 16:55:09 +0530

    missing packages

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'node_modules/pdfjs-dist/build/pdf.worker.min.mjs',
-          dest: ''
-        }
-      ]
-    })
-  ],
-})
+    plugins: [
+        react(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "node_modules/pdfjs-dist/build/pdf.worker.min.js",
+                    dest: "",
+                },
+            ],
+        }),
+    ],
+});
