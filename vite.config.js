865c09d53f2f41e4a86138ce81aaa0b7fa11e1ec
faisal-990 <sawezfaisals123@gmail.com>
2025-07-05 12:45:12 +0530

    pdf worker fix fro vercel deployent

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     viteStaticCopy({
       targets: [
         {
-          src: 'node_modules/pdfjs-dist/build/pdf.worker.min.mjs',
+          src: 'node_modules/pdfjs-dist/build/pdf.worker.mjs',
           dest: '',
           rename: 'pdf.worker.min.mjs'
         }
